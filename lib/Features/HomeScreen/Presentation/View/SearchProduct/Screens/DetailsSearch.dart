05425f2cb1b4c275b551d25041274fd8116fc7e9
ragaie-ahmed <ragaaiahmed18@gmail.com>
2024-02-10 17:27:44 +0200

    update

--- lib/Features/HomeScreen/Presentation/View/SearchProduct/Screens/DetailsSearch.dart
+++ lib/Features/HomeScreen/Presentation/View/SearchProduct/Screens/DetailsSearch.dart
@@ -25,105 +25,124 @@ class _DetailsSearchState extends State<DetailsSearch> {
 
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("تفاصيل المنتج"),
-        leading: IconButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          icon: Icon(Icons.arrow_back_ios_new_sharp),
-        ),
-        centerTitle: true,
-      ),
       body: BlocBuilder<SearchProductCubit, SearchProductState>(
         builder: (context, state) {
           var cubit = SearchProductCubit.get(context);
 
           if (state is SuccessSearch) {
-            return SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  Image.asset("Assets/Images/6770245.png"),
-                  Padding(
-                    padding: const EdgeInsets.all(15.0),
-                    child: Text(
-                      " ${cubit.searchproduct!.title}  : العنوان",
-                      style: const TextStyle(fontSize: 18, color: Colors.black),
+            return SafeArea(
+              child: SingleChildScrollView(
+                child: Column(
+
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                   SizedBox(height: 10,),
+                    Align(
+                        alignment: Alignment.center,
+                        child: Text("تفاصيل المنتج",style: TextStyle(fontSize: 20),),),
+                    Container(
+                      width: MediaQuery.of(context).size.width,
+                      height: MediaQuery.of(context).size.height / 3,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage("Assets/Images/6770245.png"),
+                        ),
+                      ),
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(15.0),
-                    child: Text(
-                      "سعر المنتج : ${cubit.searchproduct!.price}",
-                      style: const TextStyle(fontSize: 18),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text.rich(
+                          textDirection: TextDirection.rtl,
+                          TextSpan(children: [
+                        TextSpan(text: " اسم المنتج : ", style: TextStyle(color: Colors.black,fontSize: 20,fontWeight: FontWeight.bold)),
+                        TextSpan(text: " ${cubit.searchproduct!.title}", style: TextStyle(color: Colors.blueAccent,fontSize: 20)),
+                      ])),
                     ),
-                  ),
-                  const Padding(
-                    padding: EdgeInsets.all(15.0),
-                    child: Text("وصف المنتج",style: TextStyle(color: Colors.black,fontSize: 20),),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(15.0),
-                    child: Text(
-                      "${cubit.searchproduct!.description}",
-                      style: TextStyle(color: Colors.grey),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text.rich(
+                          textDirection: TextDirection.rtl,
+                          TextSpan(children: [
+                        TextSpan(text: "سعر المنتج : ", style: TextStyle(color: Colors.black,fontSize: 20,fontWeight: FontWeight.bold)),
+                        TextSpan(text: " ${cubit.searchproduct!.price}", style: TextStyle(color: Colors.blueAccent,fontSize: 20)),
+                      ])),
                     ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      SizedBox(
-                          width: 150,
-                          child: Text(
-                            "تاريخ الانشاء: ${cubit.searchproduct!.createdAt}",
-                            style:
-                                const TextStyle(overflow: TextOverflow.ellipsis),
-                          )),
-                      SizedBox(
-                          width: 170,
-                          child: Text(
-                            "اخر تحديث : ${cubit.searchproduct!.updatedAt}",
-                            style:
-                                const TextStyle(overflow: TextOverflow.ellipsis),
-                          )),
-                    ],
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-              
-              
-                  InkWell(
-                    onTap: () {
-                      Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context) {
-                        return BuildScan();
-                      },), (route) => false);
-                    },
-                    child: Align(
-                      alignment: Alignment.topCenter,
-                      child: SizedBox(
-                        width: 100.0,
-                        height: 100.0,
-                        child: Shimmer.fromColors(
-                          baseColor: Colors.black,
-                          highlightColor: Colors.grey,
-                          child: Align(
-                            alignment: Alignment.topCenter,
-                            child: Text(
-                              'رجوع',
-                              textAlign: TextAlign.center,
-                              style: TextStyle(
-                                fontSize: 40.0,
-                                fontWeight: FontWeight.bold,
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text.rich(
+                          textDirection: TextDirection.rtl,
+                          TextSpan(children: [
+                        TextSpan(text: " تاريخ الانشاء:  : ", style: TextStyle(color: Colors.black,fontSize: 20,fontWeight: FontWeight.bold)),
+                        TextSpan(text: " ${cubit.searchproduct!.createdAt}", style: TextStyle(color: Colors.blueAccent,fontSize: 20)),
+                      ])),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text.rich(
+                          textDirection: TextDirection.rtl,
+                          TextSpan(children: [
+                        TextSpan(text: " اخر تحديث : ", style: TextStyle(color: Colors.black,fontSize: 20,fontWeight: FontWeight.bold)),
+                        TextSpan(text: " ${cubit.searchproduct!.updatedAt}", style: TextStyle(color: Colors.blueAccent,fontSize: 20)),
+                      ])),
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.all(15.0),
+                      child: Text(
+                        "وصف المنتج",
+                        style: TextStyle(color: Colors.black, fontSize: 20),
+                      ),
+                    ),
+                   Padding(
+                     padding: const EdgeInsets.only(top: 5,right: 20),
+                     child: Text.rich(
+
+                         TextSpan(
+                         text: "${cubit.searchproduct!.description}",
+                         style: TextStyle(fontSize: 18),
+                       ),
+                       style: TextStyle(
+                         color:Colors.blueAccent
+                       ),
+                       textDirection: TextDirection.rtl,
+
+                     ),
+                   ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    InkWell(
+                      onTap: () {
+                        Navigator.pushAndRemoveUntil(context, MaterialPageRoute(
+                          builder: (context) {
+                            return BuildScan();
+                          },
+                        ), (route) => false);
+                      },
+                      child: Align(
+                        alignment: Alignment.topCenter,
+                        child: SizedBox(
+                          width: 100.0,
+                          height: 100.0,
+                          child: Shimmer.fromColors(
+                            baseColor: Colors.black,
+                            highlightColor: Colors.grey,
+                            child: Align(
+                              alignment: Alignment.topCenter,
+                              child: Text(
+                                'رجوع',
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                  fontSize: 40.0,
+                                  fontWeight: FontWeight.bold,
+                                ),
                               ),
                             ),
                           ),
                         ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             );
           } else if (state is ErrorSearch) {
@@ -132,12 +151,16 @@ class _DetailsSearchState extends State<DetailsSearch> {
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Text(state.error),
-                  SizedBox(height: 10,),
+                  SizedBox(
+                    height: 10,
+                  ),
                   InkWell(
                     onTap: () {
-                      Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context) {
-                        return BuildScan();
-                      },), (route) => false);
+                      Navigator.pushAndRemoveUntil(context, MaterialPageRoute(
+                        builder: (context) {
+                          return BuildScan();
+                        },
+                      ), (route) => false);
                     },
                     child: Align(
                       alignment: Alignment.topCenter,
@@ -162,7 +185,6 @@ class _DetailsSearchState extends State<DetailsSearch> {
                       ),
                     ),
                   ),
-
                 ],
               ),
             );
